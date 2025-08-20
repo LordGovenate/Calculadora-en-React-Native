fdd22c58f3b7c28fd9f2f08fe3650328ca18faef
Josue uribe ugalde <josue86443@gmail.com>
2025-08-20 07:34:08 -0600

    Agregar título y estilos a la calculadora en App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+<Text style={styles.title}>Calculadora de Kevin y Josue</Text>
+  
+      </View>
+      
     </View>
   );
 }
@@ -16,5 +18,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
 });
