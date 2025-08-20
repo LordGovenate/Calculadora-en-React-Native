308467ee7a90df7fe57b30e1a588eaaaf1771615
Josue uribe ugalde <josue86443@gmail.com>
2025-08-20 08:07:22 -0600

    Corregir estilos de texto en botones de operaciones en App.js

--- App.js
+++ App.js
@@ -7,21 +7,21 @@ export default function App() {
       <Text style={styles.title}>Calculadora de Kevin y Josue</Text>
   
       </View>
-      <View>
+      <View style={styles.botones}>
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.title}>Sumar</Text>
+          <Text style={styles.text}>Sumar</Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.title}>Restar</Text>
+          <Text style={styles.text}>Restar</Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.title}>Multiplicar</Text>
+          <Text style={styles.text}>Multiplicar</Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.title}>Dividir</Text>
+          <Text style={styles.text}>Dividir</Text>
         </TouchableOpacity>
       </View>
       
@@ -41,9 +41,18 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
   },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
 button:
 {
   backgroundColor: 'blue', 
   padding: 10, 
   borderRadius: 5},
+
+  botones: {
+    flexDirection: 'row',
+    gap: 10,
+  }
 });
